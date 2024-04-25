db81956c7ad18de62643dde1e11222e07778337a
djosedonado <djosedonado@unicesar.edu.co>
2024-04-25 00:06:55 -0500

    se realizan combios

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
+    process.env.CONNECT, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            //rejectUnauthorized: false // Para evitar errores de certificado en entornos de desarrollo, elimina esta línea en producción
+        }
+    }
+}
+);
+
+export const _sequelize = new Sequelize(
     process.env.DATABASE,
     process.env.USER_DB,
     process.env.PASSWORD,
